68e13fe363a3df9d231202fa4d142dca151f5539
mac <20j.code@gmail.com>
2024-10-16 15:33:16 +0900

    docs : 권한 검사 파라미터 주석 추가

--- src/main/java/dbdr/security/model/DbdrAuth.java
+++ src/main/java/dbdr/security/model/DbdrAuth.java
@@ -9,12 +9,11 @@ import org.springframework.security.access.prepost.PreAuthorize;
 /**
  * DbdrAuth 도메인 관련 권한 확인 어노테이션 <br>
  * <br>
- * role : 누가 접근할 수 있는가 -> 어드민을 위한 것이다 or 요양보호사를 위한 것이다<br>
+ * targetRole : 누가 접근할 수 있는가 -> 어드민을 위한 것이다 or 요양보호사를 위한 것이다<br>
  * type : 해당 메소드 파라미터 타입 ex) 요양원ID, 요양사ID, 환자ID, 차트ID 등<br>
  * id : 전달하고자하는 실제 값의 메소드 파라미터명<br>
  * <br>
- * ex) @DbdrAuth(type = AuthParam.INSTITUTION_ID, id = "institutionId")<br>
- *     public void getChart(Long institutionId) {...}<br>
+ *
  * @see dbdr.security.service.DbdrSeucrityService
  * @see AuthParam
  */
@@ -22,10 +21,9 @@ import org.springframework.security.access.prepost.PreAuthorize;
 @Retention(RetentionPolicy.RUNTIME)
 @PreAuthorize("@dbdrSecurityService.hasAccessPermission(#role,#type,#id)")
 public @interface DbdrAuth {
-    Role role();
+    Role targetRole();
     AuthParam type();
-    String id() default "";
-    //TODO : 정상작동 테스트 해봐야함. 특히 ID 부분이 String으로 넘어가는 것 유의하기
+    long id();
 }
 
 
