882b7ecc70f5e27c021ac17c0d49fb270fb992aa
mac <20j.code@gmail.com>
2024-10-16 16:07:08 +0900

    feat : Null 체크 + role 확인 메서드 추가

--- src/main/java/dbdr/security/model/BaseUserDetails.java
+++ src/main/java/dbdr/security/model/BaseUserDetails.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -14,11 +15,16 @@ import org.springframework.security.core.userdetails.UserDetails;
 @Getter
 public class BaseUserDetails implements UserDetails {
 
+    @NonNull
     private final Long id;
+    @NonNull
     private final String userLoginId;
-    private final String password;
+    @NonNull
     private final Role role;
+    @NonNull
     private final Long institutionId;
+
+    private final String password;
 
 
     @Override
@@ -36,6 +42,22 @@ public class BaseUserDetails implements UserDetails {
     @Override
     public String getUsername() {
         return userLoginId;
+    }
+
+    public boolean isAdmin(){
+        return role.equals(Role.ADMIN);
+    }
+
+    public boolean isInstitution(){
+        return role.equals(Role.INSTITUTION);
+    }
+
+    public boolean isCareworker(){
+        return role.equals(Role.CAREWORKER);
+    }
+
+    public boolean isGuardian(){
+        return role.equals(Role.GUARDIAN);
     }
 
 }
