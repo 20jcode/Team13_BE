b6486931c03b0f46f052a93948199602fd23cdd5
mac <20j.code@gmail.com>
2024-11-10 12:55:19 +0900

    test : @DbdrAuth 로직 테스트 추가

---
+++ src/test/java/dbdr/security/service/DbdrSeucrityServiceTest.java
@@ -0,0 +1,18 @@
+package dbdr.security.service;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class DbdrSeucrityServiceTest {
+
+    @InjectMocks
+    private DbdrSeucrityService dbdrSeucrityService;
+
+
+    @Test
+    void hasAcesssPermission() {
+    }
+}
